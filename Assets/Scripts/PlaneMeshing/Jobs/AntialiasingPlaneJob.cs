5e9cb4d50bad785067b82919b5756f75db28783c
amuk1989 <58622904+amuk1989@users.noreply.github.com>
2023-10-19 13:13:07 +0700

    Feature/antialiasing (#4)
    * AntialiasingPlaneJob
    
    * Update PlaneRecognizer.cs
    
    * add models
    
    * delete trash
    
    * add debug info
    
    * trashhold config
    
    * add offset

--- Assets/Scripts/PlaneMeshing/Jobs/AntialiasingPlaneJob.cs
+++ Assets/Scripts/PlaneMeshing/Jobs/AntialiasingPlaneJob.cs
@@ -10,11 +10,13 @@ namespace PlaneMeshing.Jobs
         public NativeArray<Vector3> Vertices;
         [ReadOnly] public float3 PlanePosition;
         [ReadOnly] public Quaternion PlaneRotation;
+        [ReadOnly] public float Offset;
 
         public void Execute(int index)
         {
             var position = Quaternion.Inverse(PlaneRotation) * Vertices[index];
-            position.y = (Quaternion.Inverse(PlaneRotation) * PlanePosition).y;
+            
+            position.y = (Quaternion.Inverse(PlaneRotation) * PlanePosition).y+Offset;
             Vertices[index] = PlaneRotation * position;
         }
     }
